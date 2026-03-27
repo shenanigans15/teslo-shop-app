5b1c8d44d76ea8b79c0fec4795776b88d901c5ac
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-27 20:44:08 -0300

    Update page preparations, shopLayout and Homepage Create CustomPagination, Footer, Header, Jumbotron, FilterSideBar, ProductGrid n Card

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,16 @@
+import { CustomPagination } from '@/components/custom/CustomPagination'
+import { products } from '@/mocks/products.mock'
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron'
+import { ProductsGrid } from '@/shop/components/ProductsGrid'
+
 export const HomePage = () => {
-  return <div>HomePage</div>
+  return (
+    <>
+      <CustomJumbotron title="Todos los productos" />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
+    </>
+  )
 }
