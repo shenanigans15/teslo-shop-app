2f37b05c40555253872c0973eebdf2194eb2d9ce
Adrian Fayos <adrianfayos1234@gmail.com>
2026-03-30 20:48:11 -0300

    Implement searchs, navigation, Logo, adminSideBar (with navigation), admin product and products page Design login and register, admin panel

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,13 +1,14 @@
-import { useRef, useState, type KeyboardEvent } from 'react'
-import { Search, ShoppingBag, Menu } from 'lucide-react'
+import { useRef, type KeyboardEvent } from 'react'
+import { Search } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import { useSearchParams } from 'react-router'
+import { Link, useParams, useSearchParams } from 'react-router'
+import { cn } from '@/lib/utils'
+import { CustomLogo } from '@/components/custom/CustomLogo'
 
 export const CustomHeader = () => {
-  const [cartCount] = useState(3)
-
   const [searchParams, setSearchParams] = useSearchParams()
+  const { gender } = useParams()
 
   const inputRef = useRef<HTMLInputElement>(null)
   const query = searchParams.get('query') || ''
@@ -31,41 +32,46 @@ export const CustomHeader = () => {
       <div className="container mx-auto px-4 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
-          <div className="flex items-center space-x-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-              <Menu className="h-5 w-5" />
-            </Button>
-            <h1 className="text-xl font-semibold tracking-tight">
-              TESLA STYLE
-            </h1>
-          </div>
+          <CustomLogo />
 
           {/* Navigation - Desktop */}
           <nav className="hidden md:flex items-center space-x-8">
-            <a
-              href="#"
-              className="text-sm font-medium transition-colors hover:text-primary"
+            <Link
+              to="/"
+              className={cn(
+                `text-sm font-medium transition-colors hover:text-primary`,
+                !gender ? 'underline underline-offset-4' : '',
+              )}
             >
-              Camisetas
-            </a>
-            <a
-              href="#"
-              className="text-sm font-medium transition-colors hover:text-primary"
+              Todos
+            </Link>
+            <Link
+              to="/gender/men"
+              className={cn(
+                `text-sm font-medium transition-colors hover:text-primary`,
+                gender === 'men' ? 'underline underline-offset-4' : '',
+              )}
             >
-              Sudaderas
-            </a>
-            <a
-              href="#"
-              className="text-sm font-medium transition-colors hover:text-primary"
+              Hombres
+            </Link>
+            <Link
+              to="/gender/women"
+              className={cn(
+                `text-sm font-medium transition-colors hover:text-primary`,
+                gender === 'women' ? 'underline underline-offset-4' : '',
+              )}
             >
-              Chaquetas
-            </a>
-            <a
-              href="#"
-              className="text-sm font-medium transition-colors hover:text-primary"
+              Mujeres
+            </Link>
+            <Link
+              to="/gender/kid"
+              className={cn(
+                `text-sm font-medium transition-colors hover:text-primary`,
+                gender === 'kid' ? 'underline underline-offset-4' : '',
+              )}
             >
-              Accesorios
-            </a>
+              Niños
+            </Link>
           </nav>
 
           {/* Search and Cart */}
@@ -74,6 +80,7 @@ export const CustomHeader = () => {
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                 <Input
+                  ref={inputRef}
                   placeholder="Buscar productos..."
                   className="pl-9 w-64 h-9 bg-white"
                   onKeyDown={handleSearch}
@@ -86,14 +93,17 @@ export const CustomHeader = () => {
               <Search className="h-5 w-5" />
             </Button>
 
-            <Button variant="ghost" size="icon" className="relative">
-              <ShoppingBag className="h-5 w-5" />
-              {cartCount > 0 && (
-                <span className="absolute -top-1 -right-1 h-5 w-5 rounded-full bg-primary text-primary-foreground text-xs flex items-center justify-center">
-                  {cartCount}
-                </span>
-              )}
-            </Button>
+            <Link to="/auth/login">
+              <Button variant="default" size="sm" className="ml-2">
+                Login
+              </Button>
+            </Link>
+
+            <Link to="/admin">
+              <Button variant="destructive" size="sm" className="ml-2">
+                Admin
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
