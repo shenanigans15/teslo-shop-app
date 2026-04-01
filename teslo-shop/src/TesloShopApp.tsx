c04de2ebf75f38535e9f74eb902f8bf6bfa958ae
Adrian Fayos <adrianfayos1234@gmail.com>
2026-04-01 14:19:36 -0300

    Implement login functionality

--- teslo-shop/src/TesloShopApp.tsx
+++ teslo-shop/src/TesloShopApp.tsx
@@ -3,12 +3,14 @@ import { appRouter } from './app.router'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 
 export const TesloShopApp = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster />
       <RouterProvider router={appRouter} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
