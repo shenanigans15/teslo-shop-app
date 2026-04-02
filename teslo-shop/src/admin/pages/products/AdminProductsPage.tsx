cdbf32df1629031d5163e4e9578af2b9a19db054
Adrian Fayos <adrianfayos1234@gmail.com>
2026-04-02 20:54:21 -0300

    Implement admin page pagination, search, table backend

--- teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
+++ teslo-shop/src/admin/pages/products/AdminProductsPage.tsx
@@ -1,4 +1,5 @@
 import { AdminTitle } from '@/admin/components/AdminTitle'
+import { CustomFullScreenLoading } from '@/components/custom/CustomFullScreenLoading'
 import { CustomPagination } from '@/components/custom/CustomPagination'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,10 +10,18 @@ import {
   TableBody,
   TableCell,
 } from '@/components/ui/table'
-import { PlusIcon } from 'lucide-react'
+import { useProducts } from '@/shop/hooks/useProducts'
+import { PencilIcon, PlusIcon } from 'lucide-react'
 import { Link } from 'react-router'
+import { currencyFormatter } from '@/lib/currency-formatter'
 
 export const AdminProductsPage = () => {
+  const { data, isLoading } = useProducts()
+
+  if (isLoading) {
+    return <CustomFullScreenLoading />
+  }
+
   return (
     <>
       <div className="flex justify-between items-center">
@@ -35,7 +44,6 @@ export const AdminProductsPage = () => {
       <Table className="bg-white p-10 shadow-xs border-gray-200 mb-10">
         <TableHeader>
           <TableRow>
-            <TableHead className="w-25">ID</TableHead>
             <TableHead>Imagen</TableHead>
             <TableHead>Nombre</TableHead>
             <TableHead>Precio</TableHead>
@@ -46,29 +54,38 @@ export const AdminProductsPage = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>
-              <img
-                src="https://placehold.co/250x250"
-                alt="Product"
-                className="w-20 h-20 object-cover rounded-md"
-              />
-            </TableCell>
-            <TableCell>Producto 1</TableCell>
-            <TableCell>$250.00</TableCell>
-            <TableCell>Categoría 1</TableCell>
-            <TableCell>100</TableCell>
-            <TableCell>XS,S,M,L</TableCell>
-            <TableCell className="text-right">
-              <Link to={`t-shirt-teslo`}>Editar</Link>
-              {/* <Link to={`admin/products/t-shirt-teslo`}>Editar</Link> */}
-            </TableCell>
-          </TableRow>
+          {data!.products.map((product) => (
+            <TableRow>
+              <TableCell>
+                <img
+                  src={product.images[0]}
+                  alt={product.title}
+                  className="w-20 h-20 object-cover rounded-md"
+                />
+              </TableCell>
+
+              <Link
+                to={`/admin/products/${product.id}`}
+                className="hover:text-blue-500 underline"
+              >
+                <TableCell>{product.title}</TableCell>
+              </Link>
+
+              <TableCell>{currencyFormatter(product.price)}</TableCell>
+              <TableCell>{product.gender}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.sizes.join(' · ')}</TableCell>
+              <TableCell className="text-right">
+                <Link to={`/admin/products/${product.id}`}>
+                  <PencilIcon className="w-4 h-4 text-blue-500" />
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
 
-      <CustomPagination totalPages={10} />
+      <CustomPagination totalPages={data?.pages || 0} />
     </>
   )
 }
